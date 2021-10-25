199c3179b4180d2222478ac10f2c13eb2a95f1bd
Satoru Nakamura <na.kamura.1263@gmail.com>
2021-10-25 14:40:02 +0900

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,8 +1,8 @@
 set -e
 git pull
-cd scripts
-python 02_convertTei2Curation.py
-python 12_convertTei2GSheets.py
+# cd scripts
+# python 02_convertTei2Curation.py
+# python 12_convertTei2GSheets.py
 cd ../
 git add .
 git commit -a -m "update"
